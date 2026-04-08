491e58fb16d2c3fb249aa6285e31e8ea321b7518
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2026-04-07 19:37:14 -0700

    Install nerd font for terminal glyphs

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -9,8 +9,23 @@
   home.packages = with pkgs; [
     cargo
     neovim
+    nerd-fonts.fira-code
     rustc
     uv
     python312
   ];
+
+  xdg.configFile."fontconfig/conf.d/10-monospace-nerd-font.conf".text = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+    <fontconfig>
+      <alias>
+        <family>monospace</family>
+        <prefer>
+          <family>FiraCode Nerd Font Mono</family>
+          <family>FiraCode Nerd Font</family>
+        </prefer>
+      </alias>
+    </fontconfig>
+  '';
 }
